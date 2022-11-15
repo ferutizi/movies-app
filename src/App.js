21208dd7551dee1b0e89dd52b58eaf06a741edfc
ferutizi <fer.utizi@gmail.com>
2022-11-15 16:21:59 -0300

    map movies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
-import { useEffect } from 'react';
-
+import { useEffect, useState } from 'react';
 
 function App() {
   const ApiKey = '436298427a108d3cea382bfb7e2d4fa0';
@@ -8,18 +7,27 @@ function App() {
   const consumirApi = useEffect(() => {
     fetchAPI();
   }, [])
+
+  const [movies, setMovies] = useState({});
   
 
   const fetchAPI = async () => {
     const res = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${ApiKey}`);
     const data = await res.json();
-    console.log(data);
+    setMovies(data);
   }
 
+  console.log(movies.results)
+
   return (
-    <div className="App">
-      <h1>Movies</h1>
-    </div>
+    <>
+      {movies.results ? 
+        <div className="App">
+          <h1>Movies</h1>
+            {movies.results.map(item => <p>{item.original_title}</p>)}
+        </div>
+      : <p>no</p>}
+    </>
   );
 }
 
