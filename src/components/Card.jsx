904fe5d16497f6170ab9374d1bd233288926f697
ferutizi <fer.utizi@gmail.com>
2022-11-17 14:17:42 -0300

    estilo hover card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,10 +9,10 @@ const Card = ({ movie, movies }) => {
             {movies.results ?
                 <div className='card__container'>
                     <img src={`${base_url}/${size_url}/${movie.poster_path}`} alt={movie.title}></img>
-                    <p className='card__arrow'>❯</p>
                     <div className='card__description'>
                         <h2 className='description__title'>{movie.title.substr(0, 32, '...')}</h2>
                         <p className='description__text'>{movie.overview.substr(0, 100)} ...</p>
+                        <div className='card__arrow'>❯</div>
                     </div>
                 </div>
             : null}
